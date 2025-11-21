fc62238ba028b7cab2d26d5ea2513e48c5f3407a
J Mandon <78100569+JuanMandon@users.noreply.github.com>
2025-11-20 19:59:00 -0500

    Playable build

--- Assets/_Game/_Scripts/Interaction/IInteractable.cs
+++
@@ -1,8 +0,0 @@
-/// <summary>
-/// Contract for any object that can be interacted with via the interaction system.
-/// </summary>
-public interface IInteractable
-{
-    void Interact();
-    string GetInteractName();
-}
